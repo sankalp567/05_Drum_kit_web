53ec55970a26e19857ad1b8f86eb42d41b167f1f
Sankalp Turankar <118650244+sankalp567@users.noreply.github.com>
2023-03-21 18:24:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,30 @@
-// document.querySelector(".w").addEventListener("click", function () {
-//     // this.Style.color="white";
-//     new Audio("sounds/tom-1.mp3").play();
-// })
 
-// document.querySelector(".a").addEventListener("click", function () {
-//     new Audio("sounds/tom-2.mp3").play();
-// })
-
-// document.querySelector(".s").addEventListener("click", function () {
-//     new Audio("sounds/tom-3.mp3").play();
-// })
-
-// document.querySelector(".d").addEventListener("click", function () {
-//     new Audio("sounds/tom-4.mp3").play();
-// })
-
-// document.querySelector(".j").addEventListener("click", function () {
-//     new Audio("sounds/snare.mp3").play();
-// })
-
-// document.querySelector(".k").addEventListener("click", function () {
-//     new Audio("sounds/crash.mp3").play();
-// })
-
-// document.querySelector(".l").addEventListener("click", function () {
-//     new Audio("sounds/kick-bass.mp3").play();
-// })
-
-// WE CAN USING AS ABOVE AS WELL BUT IN BELOW WE USE WHAT WE HAVE LEARN IN JAVASCRIPT AND IT IS EFFICIEANT
-
-// THIS  IS CLICK EVENTLISTENER
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         var buttoninnerhtml = this.innerHTML;
         switch (buttoninnerhtml) {
             case "w":
-                new Audio("sounds/tom-1.mp3").play();
+                new Audio("tom-1.mp3").play();
                 break;
             case "a":
-                new Audio("sounds/tom-2.mp3").play();
+                new Audio("tom-2.mp3").play();
                 break;
             case "s":
-                new Audio("sounds/tom-3.mp3").play();
+                new Audio("tom-3.mp3").play();
                 break;
 
             case "d":
-                new Audio("sounds/tom-4.mp3").play();
+                new Audio("tom-4.mp3").play();
                 break;
             case "j":
-                new Audio("sounds/snare.mp3").play();
+                new Audio("snare.mp3").play();
                 break;
 
             case "k":
-                new Audio("sounds/crash.mp3").play();
+                new Audio("crash.mp3").play();
                 break;
             case "l":
-                new Audio("sounds/kick-bass.mp3").play();
+                new Audio("kick-bass.mp3").play();
                 break;
             default: console.log(buttoninnerhtml);
         }
@@ -69,61 +38,34 @@ document.addEventListener("keydown", function (event) {
     switch (event.key) {
         // here key is special word for keydown event
         case "w":
-            new Audio("sounds/tom-1.mp3").play();
+            new Audio("tom-1.mp3").play();
             break;
         case "a":
-            new Audio("sounds/tom-2.mp3").play();
+            new Audio("tom-2.mp3").play();
             break;
         case "s":
-            new Audio("sounds/tom-3.mp3").play();
+            new Audio("tom-3.mp3").play();
             break;
 
         case "d":
-            new Audio("sounds/tom-4.mp3").play();
+            new Audio("tom-4.mp3").play();
             break;
         case "j":
-            new Audio("sounds/snare.mp3").play();
+            new Audio("snare.mp3").play();
             break;
 
         case "k":
-            new Audio("sounds/crash.mp3").play();
+            new Audio("crash.mp3").play();
             break;
         case "l":
-            new Audio("sounds/kick-bass.mp3").play();
+            new Audio("kick-bass.mp3").play();
             break;
         default: console.log(event.key);
     }
     keyanimetion(event.key);
 
 })
-// function makedound(key){
-//     switch (key) {
-//         case "w":
-//             new Audio("sounds/tom-1.mp3").play();
-//             break;
-//         case "a":
-//             new Audio("sounds/tom-2.mp3").play();
-//             break;
-//         case "s":
-//             new Audio("sounds/tom-3.mp3").play();
-//             break;
 
-//         case "d":
-//             new Audio("sounds/tom-4.mp3").play();
-//             break;
-//         case "j":
-//             new Audio("sounds/snare.mp3").play();
-//             break;
-
-//         case "k":
-//             new Audio("sounds/crash.mp3").play();
-//             break;
-//         case "l":
-//             new Audio("sounds/kick-bass.mp3").play();
-//             break;
-//         default:  console.log(buttoninnerhtml);
-//     }
-// }
 function keyanimetion(currentkey) {
     var activekey = document.querySelector("." + currentkey);
     activekey.classList.add("pressed");
